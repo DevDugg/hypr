0095a081f32b4de5853fec3bce927a07169ec2d6
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-07-14 19:42:39 +0500

    nav data added

---
+++ src/database/navigation.data.ts
@@ -0,0 +1,18 @@
+export const navData = [
+  {
+    view: 'Work',
+    path: '/work',
+  },
+  {
+    view: 'About',
+    path: '/about',
+  },
+  {
+    view: 'News',
+    path: '/news',
+  },
+  {
+    view: 'Our Creators',
+    path: '/creators',
+  },
+];
