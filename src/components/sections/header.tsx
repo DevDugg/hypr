29ccecda397aa88e7c8b3de28263e53a8eb7c49f
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-07-14 19:42:06 +0500

    desktop version

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,14 +1,27 @@
+import { navData } from '@/database/navigation.data';
 import Container from '../layout/container';
+import Link from 'next/link';
+import { Button } from '../ui/button';
 
 export const Header = () => {
   return (
     <Container className="">
-      <header className="pt-6 pb-10 flex items-center justify-between text-WHITE">
+      <header className="pt-6 pb-10 lg:py-[26px] flex items-center justify-between text-WHITE">
         <div className="uppercase text-[40px] leading-none">HYPR</div>
 
-        <div className="size-6 flex flex-col justify-center items-center gap-1 cursor-pointer">
-          <div className="w-[18px] h-0.5 bg-WHITE"></div>
-          <div className="w-[18px] h-0.5 bg-WHITE"></div>
+        <nav className="hidden lg:flex gap-10 border-WHITE border-[1px] text-[18px] leading-none px-6 py-4 rounded-full">
+          {navData.map((item, i) => (
+            <Link key={i} href={''}>
+              {item.view}
+            </Link>
+          ))}
+        </nav>
+
+        <Button className="hidden lg:flex">Contact us</Button>
+
+        <div className="size-6 flex flex-col justify-center items-center gap-1 cursor-pointer lg:hidden">
+          <div className="w-[18px] h-0.5 bg-WHITE" />
+          <div className="w-[18px] h-0.5 bg-WHITE" />
         </div>
       </header>
     </Container>
