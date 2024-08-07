3dda8757bb2eefd191035cca8f386153bbe23e80
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-07 06:48:47 +0500

    added animation word by word about section

---
+++ src/components/sections/about/character.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { PropsWithChildren, ReactNode, useEffect, useRef } from "react";
+import { useScroll, motion, useTransform } from "framer-motion";
+
+export default function Paragraph({ value }: { value: string }) {
+  const element = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start 0.9", "start 0.25"],
+  });
+
+  const words = value.split("");
+
+  return (
+    <p className="text-[20px]" ref={element}>
+      {words.map((item, i) => {
+        const start = i / words.length;
+        const end = start + 1 / words.length;
+        console.log(start, end);
+        return (
+          <Word range={[start, end]} progress={scrollYProgress}>
+            {item}
+          </Word>
+        );
+      })}
+    </p>
+  );
+}
+
+const Word = ({
+  children,
+  range,
+  progress,
+}: {
+  children: ReactNode;
+  range: number[];
+  progress: any;
+}) => {
+  const opacity = useTransform(progress, range, [0.15, 1]);
+
+  return (
+    <motion.span
+      style={{ opacity }}
+      className="text-[20px] xl:text-[50px] first:ml-[40%] mr-[0.5px]"
+    >
+      {children}
+    </motion.span>
+  );
+};
--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -1,49 +1,34 @@
-'use client';
-
-import SectionLayout from '@/components/layout/section-layout';
-import { grotesk } from '@/lib/fonts';
-import { cn } from '@/lib/utils';
-import { useEffect, useRef } from 'react';
-import { useScroll, useTransform } from 'framer-motion';
-import clsx from 'clsx';
-import Container from '@/components/layout/container';
-import SectionName from '@/components/titles/section-name';
+import SectionLayout from "@/components/layout/section-layout";
+import { grotesk } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import { useEffect, useRef } from "react";
+import { useScroll, useTransform } from "framer-motion";
+import clsx from "clsx";
+import Container from "@/components/layout/container";
+import SectionName from "@/components/titles/section-name";
+import Paragraph from "./character";
 
 const About = () => {
   const paragraph = `HYPR agency is a branding and digital marketing studio from Australia.
-          Partnering with companies that need to transform their brand, attract
-          the right audience, and radically stand out. We’re a nimble team of
-          designers and creative developers, focused on creating uniquely human
-          and culturally informed. Read more`;
-
-  const words = paragraph.split('');
-
-  const start = words.length;
-  const end = start + 1 / words.length;
-  const amount = end - start;
-  const step = amount / words.length;
+        Partnering with companies that need to transform their brand, attract
+        the right audience, and radically stand out. We’re a nimble team of
+        designers and creative developers, focused on creating uniquely human
+        and culturally informed. Read more`;
 
   return (
     <section>
       <Container className="relative">
-        <SectionName className="absolute top-2 xl:top-14 left-0">(about)</SectionName>
+        <SectionName className="absolute top-2 xl:top-14 left-0">
+          (about)
+        </SectionName>
         <div
           className={cn(
-            'about-text flex transition-colors text-WHITE duration-200 relative',
-            grotesk.className,
-          )}>
+            "about-text flex transition-colors text-WHITE duration-200 relative",
+            grotesk.className
+          )}
+        >
           <div className="font-semibold text-[20px] xl:text-[56px]">
-            {words.map((word, i) => {
-              // const started = end + step * i;
-              // const ended = started + step * i + 1;
-              // const opacity = useTransform(scrollYProgress, [start, end], [0, 1]);
-
-              return (
-                <span key={i} className={clsx('first:ml-[40%]')}>
-                  {word}
-                </span>
-              );
-            })}
+            <Paragraph value={paragraph} />
           </div>
         </div>
       </Container>
