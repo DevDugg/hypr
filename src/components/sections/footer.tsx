7929a3ac2a207c63e9fbdb9be3c69a82364d85f9
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-08 18:39:01 +0500

    bit changes

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -3,17 +3,18 @@ import Container from '../layout/container';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { grotesk } from '@/lib/fonts';
-import { ArrowBigUp, ArrowUp10Icon, ArrowUpIcon, ArrowUpToLine } from 'lucide-react';
+import { ArrowUpIcon } from 'lucide-react';
+import Logo from '../layout/logo';
 
 export const Footer = () => {
   return (
-    <footer className="text-WHITE bg-BLACK pt-[100px] pb-9 mt-[100px] xl:mt-[400px]">
+    <footer className="text-WHITE bg-BLACK pt-[100px] pb-9">
       <Container className="flex flex-col gap-[100px]">
-        <div className={cn('flex justify-between', grotesk.className)}>
+        <div className={cn('flex gap-[25%]', grotesk.className)}>
           <div>
-            <p className="mb-10 font-bold text-[20px]">(Sitemap)</p>
+            <p className="mb-10 font-bold">(Sitemap)</p>
 
-            <div className="flex flex-col gap-2 font-bold text-[30px]">
+            <div className="flex flex-col gap-2 font-bold h5">
               {footerList.map((item, i) => (
                 <Link key={i} href={item.link}>
                   {item.name}
@@ -23,9 +24,9 @@ export const Footer = () => {
           </div>
 
           <div>
-            <p className="mb-10 font-bold text-[20px]">(Socials)</p>
+            <p className="mb-10 font-bold">(Socials)</p>
 
-            <div className="flex flex-col gap-2 font-bold text-[30px]">
+            <div className="flex h5 flex-col gap-2 font-bold">
               {footerList2.map((item, i) => (
                 <Link key={i} href={item.link}>
                   {item.name}
@@ -33,18 +34,22 @@ export const Footer = () => {
               ))}
             </div>
           </div>
-
-          <div className=""></div>
         </div>
 
         <div className="flex justify-between items-center">
-          <div className="text-[15vw] mb-20">hypr</div>
+          <Logo w={900} h={350} className="flex-[0_1_45%]" />
 
-          <div className={cn('flex items-center justify-center gap-1', grotesk.className)}>
+          <div
+            className={cn(
+              'flex items-center flex-[0_1_20%] justify-center gap-1',
+              grotesk.className,
+            )}>
             Back to top <ArrowUpIcon size={12} className="-mb-1" />
           </div>
 
-          <div className={cn(grotesk.className, 'font-bold')}>Copyright © hypr 2023</div>
+          <div className={cn(grotesk.className, 'font-bold flex-[0_1_20%]')}>
+            Copyright © hypr 2023
+          </div>
         </div>
       </Container>
     </footer>
