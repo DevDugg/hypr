adf5b01903b7163ce1070b41afe37771ac29d912
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-08 18:40:47 +0500

    fz changed

--- src/components/titles/section-subtitle.tsx
+++ src/components/titles/section-subtitle.tsx
@@ -1,5 +1,5 @@
-import { grotesk } from "@/lib/fonts";
-import { cn } from "@/lib/utils";
+import { grotesk } from '@/lib/fonts';
+import { cn } from '@/lib/utils';
 
 const SectionSubtitle = ({
   className,
@@ -8,16 +8,7 @@ const SectionSubtitle = ({
   className?: string;
   children: React.ReactNode;
 }) => {
-  return (
-    <h5
-      className={cn(
-        grotesk.className,
-        "text-[calc(1.1vh+1.1vw)] leading-[140%] font-medium"
-      )}
-    >
-      {children}
-    </h5>
-  );
+  return <h5 className={cn(grotesk.className, 'h5 leading-[140%] font-medium')}>{children}</h5>;
 };
 
 export default SectionSubtitle;
