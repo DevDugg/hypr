74bb65a22bb4e113a01aaa9fd4e4be8b00fa5e1e
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-09 18:12:10 +0500

    animation while scroll

--- src/components/sections/our-services/services-block.tsx
+++ src/components/sections/our-services/services-block.tsx
@@ -2,46 +2,36 @@
 
 import { servicesData } from '@/database/services.data';
 import { ServiceCard } from './service-card';
-import { useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
+import { delay, motion } from 'framer-motion';
+import { useMotionValueEvent, useScroll } from 'framer-motion';
 
 const ServicesBlock = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const element = useRef(null);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const viewportHeight = window.innerHeight;
-  //     servicesData.forEach((_, index) => {
-  //       const element = document.getElementById(`element-${index}`);
-  //       if (element) {
-  //         const rect = element.getBoundingClientRect();
-  //         const elementCenter = rect.top + rect.height / 2;
-  //         if (
-  //           elementCenter >= viewportHeight / 2 - 50 &&
-  //           elementCenter <= viewportHeight / 2 + 50
-  //         ) {
-  //           setActiveIndex(index);
-  //         }
-  //       }
-  //     });
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, [servicesData]);
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ['end end', 'start start'],
+  });
 
   return (
-    <div className="flex flex-col gap-4 xl:gap-6 mt-2 xl:mt-6">
-      {servicesData.map((item, i) => (
-        <ServiceCard
-          i={i}
-          setActiveIndex={setActiveIndex}
-          active={activeIndex === i}
-          key={i}
-          {...item}
-        />
-      ))}
+    <div className="flex flex-col gap-4 xl:gap-6 mt-2 xl:mt-6" ref={element}>
+      {servicesData.map((item, i) => {
+        const start = i / servicesData.length;
+        const end = start + 1 / servicesData.length;
+        return (
+          <ServiceCard
+            i={i}
+            range={[start, end]}
+            setActiveIndex={setActiveIndex}
+            progress={scrollYProgress}
+            active={activeIndex === i}
+            key={i}
+            {...item}
+          />
+        );
+      })}
     </div>
   );
 };
