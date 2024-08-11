7e5f1a094a31d19b8ad5a2ca4e10d73b205a3cb4
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-11 13:22:54 +0500

    nothing

--- src/components/sections/socials/index.tsx
+++ src/components/sections/socials/index.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import Container from '@/components/layout/container';
-import { socialsData } from '@/database/socials.data';
-import clsx from 'clsx';
-import { motion } from 'framer-motion';
+import Container from "@/components/layout/container";
+import { socialsData } from "@/database/socials.data";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const Socials = () => {
   return (
@@ -12,15 +12,19 @@ const Socials = () => {
         <motion.div
           key={i}
           initial={
-            (i === 0 && { translateY: 360 }) ||
-            (i === 1 && { translateY: 240 }) ||
-            (i === 2 && { translateY: 120 })
+            (i === 0 && { translateY: 340 }) ||
+            (i === 1 && { translateY: 220 }) ||
+            (i === 2 && { translateY: 100 })
           }
-          className={clsx('text-[calc(4.5vw+4.5vh)] rounded-[40px_40px_0_0] pt-20', {
-            'bg-OFF_WHITE ': i === 0,
-            'bg-[#87ADF9]': i === 1,
-            'bg-SECONDARY': i === 2,
-          })}>
+          className={clsx(
+            "text-[calc(4.5vw+4.5vh)] rounded-[40px_40px_0_0] pt-20 relative -z-10",
+            {
+              "bg-OFF_WHITE ": i === 0,
+              "bg-[#87ADF9]": i === 1,
+              "bg-SECONDARY": i === 2,
+            }
+          )}
+        >
           <Container>{item.name}</Container>
         </motion.div>
       ))}
