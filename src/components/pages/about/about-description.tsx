b88c51059f5f97113c71a7992bd09922d9d8447e
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-11 13:21:37 +0500

    description sectiion process about page

---
+++ src/components/pages/about/about-description.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/layout/container";
+
+const AboutDescription = () => {
+  return (
+    <section className="bg-ACCENT">
+      <Container></Container>
+    </section>
+  );
+};
+
+export default AboutDescription;
