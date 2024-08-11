7f58a59ec0a36d99b6879b2ee51b1ca7dea5a1a9
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-11 13:21:50 +0500

    hero of about page

---
+++ src/components/pages/about/about-hero.tsx
@@ -0,0 +1,35 @@
+import Container from "@/components/layout/container";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const AboutHero = () => {
+  return (
+    <section className="text-WHITE">
+      <Container>
+        <h1 className="text-[calc(8vw+4vh)] xl:text-[calc(16vh+12vw)] text-center text-ACCENT">
+          about us
+        </h1>
+
+        <div className="flex items-start justify-between">
+          <div className="flex flex-col gap-2 flex-[0_0_30%]">
+            <Button className="about-btn">marketing</Button>
+            <Button className="about-btn ml-[20%]">social media</Button>
+          </div>
+
+          <div className="text-center text-[calc(1.5vh+1.5vw)]">
+            strategic, results-oriented influencer collaborations
+          </div>
+
+          <div className="flex flex-col gap-2 items-end flex-[0_0_30%]">
+            <Button className="about-btn">marketing</Button>
+            <Button className="about-btn mr-[20%]">social media</Button>
+          </div>
+        </div>
+
+        <Image src="" alt="image" />
+      </Container>
+    </section>
+  );
+};
+
+export default AboutHero;
