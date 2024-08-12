acd031e2eb0e69225c038ee780153e58bcb78767
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-12 14:29:56 +0500

    none

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import { grotesk } from '@/lib/fonts';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center size24 whitespace-nowrap rounded-md font-bold ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300',
