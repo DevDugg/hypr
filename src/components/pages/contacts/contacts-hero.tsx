fdb5ebd5ee08b39218d5086b652698570470389f
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-12 14:29:18 +0500

    font-size changed

--- src/components/pages/contacts/contacts-hero.tsx
+++ src/components/pages/contacts/contacts-hero.tsx
@@ -7,7 +7,7 @@ const ContactsHero = () => {
   return (
     <section>
       <Container>
-        <h1 className="text-[18.5vw] text-ACCENT text-center mb-[15vw]">contacts</h1>
+        <h1 className="text-[18.5vw] text-ACCENT text-center mb-[5vw]">contacts</h1>
 
         <p className={cn('text-WHITE size40 w-[50%] font-semibold mb-[4vw]', grotesk.className)}>
           Let's get this conversation started. Use the form below or send us an email. We will
