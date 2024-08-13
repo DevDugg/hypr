c0c9590edca6d3661af6d129d4ebba0c31786bc4
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-13 17:29:02 +0500

    vw added

--- src/components/titles/section-title.tsx
+++ src/components/titles/section-title.tsx
@@ -1,7 +1,17 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-const SectionTitle = ({ className, children }: { className?: string; children: React.ReactNode }) => {
-  return <h3 className={cn(className, "title text-WHITE leading-none font-normal uppercase")}>{children}</h3>;
+const SectionTitle = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <h3 className={cn(className, 'size80 text-WHITE leading-none font-normal uppercase')}>
+      {children}
+    </h3>
+  );
 };
 
 export default SectionTitle;
