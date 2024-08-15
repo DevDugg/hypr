a5dfea381b04baef82cc422a6bb0ac3cd93dac63
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-15 18:33:05 +0500

    sizes changed

--- src/components/pages/news/news-items.tsx
+++ src/components/pages/news/news-items.tsx
@@ -9,7 +9,12 @@ const NewsItems = () => {
     <section className="mb-[20vw]">
       <div className="grid grid-cols-3 gap-y-[1.25vw] gap-x-[0.9vw] mb-[2.4vw]">
         {newsData.map((item, i) => (
-          <NewsCard key={i} {...item} className="text-WHITE" imgClassName="w-full" />
+          <NewsCard
+            key={i}
+            {...item}
+            className="text-WHITE !w-[30.67vw]"
+            imgClassName="w-full !h-[33.33vw]"
+          />
         ))}
       </div>
 
