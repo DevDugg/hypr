f43391b05675187dbf8d164a10dffcb547cc0a15
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-08-15 20:47:15 +0500

    animations

--- src/components/sections/about/character.tsx
+++ src/components/sections/about/character.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import { ReactNode, useRef } from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { MotionValue, motion, useMotionValue, useScroll, useTransform } from "framer-motion";
+import { ReactNode, useRef } from "react";
 
 export default function Paragraph({ value }: { value: string }) {
   const element = useRef(null);
   const { scrollYProgress } = useScroll({
     target: element,
-    offset: ['start 0.9', 'start 0.2'],
+    offset: ["start 0.5", "start 0.2"],
   });
 
-  const words = value.split('');
+  const words = value.split("");
 
   return (
     <p className="text-[3.33vw]" ref={element}>
@@ -18,7 +18,7 @@ export default function Paragraph({ value }: { value: string }) {
         const start = i / words.length;
         const end = start + 1 / words.length;
         return (
-          <Word range={[start, end]} progress={scrollYProgress} key={i}>
+          <Word range={[start, end]} progress={scrollYProgress} key={i} i={i}>
             {item}
           </Word>
         );
@@ -31,15 +31,21 @@ const Word = ({
   children,
   range,
   progress,
+  i,
 }: {
   children: ReactNode;
   range: number[];
-  progress: any;
+  progress: MotionValue<number>;
+  i: number;
 }) => {
   const opacity = useTransform(progress, range, [0.15, 1]);
-
-  return (
-    <motion.span style={{ opacity }} className="size64 first:ml-[32vw] mr-[0.5px]">
+  const paddingLeft = useTransform(progress, [0, 1], [0, 24]);
+  return i === 0 ? (
+    <motion.span style={{ opacity, paddingLeft }} className="size64 ml-[25vw] mr-[0.5px]">
+      {children}
+    </motion.span>
+  ) : (
+    <motion.span style={{ opacity }} className="size64 mr-[0.5px]">
       {children}
     </motion.span>
   );
