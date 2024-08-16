15b1fda8647c77e223bd53aa7f8db2db1f7b4823
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-17 00:38:43 +0500

    z-index added

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { footerList, footerList2 } from '@/database/footer.data';
-import Container from '../layout/container';
-import Link from 'next/link';
-import { cn } from '@/lib/utils';
-import { grotesk } from '@/lib/fonts';
-import { ArrowUpIcon } from 'lucide-react';
-import Logo from '../layout/logo';
+import { footerList, footerList2 } from "@/database/footer.data";
+import Container from "../layout/container";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { grotesk } from "@/lib/fonts";
+import { ArrowUpIcon } from "lucide-react";
+import Logo from "../layout/logo";
 
 export const Footer = () => {
   return (
-    <footer className="text-WHITE bg-BLACK pt-[5vw] pb-[2.5vw]">
+    <footer className="text-WHITE bg-BLACK pt-[5vw] pb-[2.5vw] relative z-10">
       <Container className="flex flex-col gap-[10vw]">
-        <div className={cn('flex gap-[24.27vw]', grotesk.className)}>
+        <div className={cn("flex gap-[24.27vw]", grotesk.className)}>
           <div>
             <p className="mb-[2vw] font-bold size24">(Sitemap)</p>
 
@@ -44,13 +44,19 @@ export const Footer = () => {
           <div className="flex gap-[13.69vw] flex-[1_0_34.58vw]">
             <div
               className={cn(
-                'flex items-center flex-[0_1_auto] cursor-pointer font-bold size24 justify-center gap-[0.2vw]',
-                grotesk.className,
-              )}>
+                "flex items-center flex-[0_1_auto] cursor-pointer font-bold size24 justify-center gap-[0.2vw]",
+                grotesk.className
+              )}
+            >
               Back to top <ArrowUpIcon className="-mb-[0.2vw] size-[1.25vw]" />
             </div>
 
-            <div className={cn(grotesk.className, 'font-bold size24 flex-[0_1_auto]')}>
+            <div
+              className={cn(
+                grotesk.className,
+                "font-bold size24 flex-[0_1_auto]"
+              )}
+            >
               Copyright © hypr 2023
             </div>
           </div>
