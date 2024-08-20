fd32c644a8f226e9ddede10fe257f5d9c41f03c2
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-08-21 01:42:36 +0500

    creators adjust

--- src/components/sections/creators/creators-block.tsx
+++ src/components/sections/creators/creators-block.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import "swiper/css";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import { Button } from "@/components/ui/button";
 import { CreatorsCard } from "./creators-card";
-import { FreeMode } from "swiper/modules";
 import { cn } from "@/lib/utils";
 import { creatorsData } from "@/database/creators.data";
 import { grotesk } from "@/lib/fonts";
@@ -14,33 +9,13 @@ import { grotesk } from "@/lib/fonts";
 const CreatorsBlock = () => {
   return (
     <>
-      <Swiper
-        modules={[FreeMode]}
-        freeMode
-        spaceBetween={"0.5vw"}
-        slidesPerView={1.8}
-        breakpoints={{
-          640: {
-            slidesPerView: 3,
-          },
-          1024: {
-            slidesPerView: 6,
-          },
-        }}
-        className="mt-[1.66vw]"
-      >
+      <div className="mt-[1.66vw] flex gap-[0.5vw]">
         {creatorsData.map((item, i) => (
-          <SwiperSlide key={i}>
-            <CreatorsCard {...item} />
-          </SwiperSlide>
+          <CreatorsCard {...item} key={i} />
         ))}
-      </Swiper>
+      </div>
 
-      <Button
-        className={cn(grotesk.className, "w-full mt-[1.66vw] xl:mt-[4.16vw]")}
-      >
-        See all creators
-      </Button>
+      <Button className={cn(grotesk.className, "w-full mt-[1.66vw] xl:mt-[4.16vw]")}>See all creators</Button>
     </>
   );
 };
--- src/components/sections/creators/creators-card.tsx
+++ src/components/sections/creators/creators-card.tsx
@@ -13,8 +13,8 @@ interface IProps {
 
 export const CreatorsCard = ({ img, title, nickname, socials }: IProps) => {
   return (
-    <div className="w-[12.5vw]">
-      <Image src={img} alt="news image" width={240} height={288} className="w-full h-[15vw]" />
+    <div className="w-full">
+      <Image src={img} alt="news image" width={240} height={288} />
 
       <div className="mt-[0.625vw] mb-[0.416vw] text-[0.9375vw] monument">{title}</div>
       <p className={cn("leading-[140%] text-[0.83vw] mb-[0.416vw] font-normal", grotesk.className)}>{nickname}</p>
