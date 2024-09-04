2aa86549e5fd50d24945f2af64c1a6ca69d42c54
DevDugg <mr.aydogdy02@gmail.com>
2024-09-05 00:23:36 +0500

    hero section fully animated

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -3,6 +3,8 @@ import Container from "@/components/layout/container";
 import CustomButton from "@/components/shared/custom-button";
 import HeroText from "./hero-text";
 import Image from "next/image";
+import { Link } from "next-view-transitions";
+import Reveal from "@/components/animations/reveal";
 import { cn } from "@/lib/utils";
 import { grotesk } from "@/lib/fonts";
 import { heroImgData } from "@/database/hero.data";
@@ -14,14 +16,15 @@ export const Hero = () => {
 
       <Container className="grid grid-cols-3 sm:grid-cols-6 sm:gap-[0.52vw] gap-[1.28vw]">
         {heroImgData.map((item, i) => (
-          <Image
-            key={i}
-            src={item.img}
-            alt="img"
-            width={291}
-            height={400}
-            className="w-[33.33vw] h-full sm:w-[15.15vw] sm:h-[20.83vw]"
-          />
+          <Reveal key={i} delay={0.5 + i * 0.1}>
+            <Image
+              src={item.img}
+              alt="img"
+              width={291}
+              height={400}
+              className="w-[33.33vw] h-full sm:w-[15.15vw] sm:h-[20.83vw]"
+            />
+          </Reveal>
         ))}
       </Container>
 
@@ -29,16 +32,19 @@ export const Hero = () => {
         <AnimatedParagraph className="uppercase text-WHITE w-full leading-none mb-[2.56vw] sm:mb-[0.83vw] text-center text-[4vw] sm:size72 monument">
           strategic, results-oriented influencer collaborations
         </AnimatedParagraph>
-        <CustomButton fullWidth>
-          <div
-            className={cn(
-              "hidden lg:flex font-bold !text-[0.9375vw] !px-[2.08vw] leading-[100%] rounded-[10.41vw]",
-              grotesk.className,
-            )}
-          >
-            Contact us
-          </div>
-        </CustomButton>
+        <Reveal animateOnView type="opacity">
+          <CustomButton fullWidth>
+            <Link
+              href="/contact"
+              className={cn(
+                "hidden lg:flex font-bold !text-[0.9375vw] !px-[2.08vw] leading-[100%] rounded-[10.41vw]",
+                grotesk.className,
+              )}
+            >
+              Contact us
+            </Link>
+          </CustomButton>
+        </Reveal>
       </Container>
     </section>
   );
