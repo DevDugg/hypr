c2bb186392bfeaa0f655a84279e5e2b993a5ea67
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-09-05 17:43:02 +0500

    mobile version

--- src/components/pages/dynamic/dynamic-hero.tsx
+++ src/components/pages/dynamic/dynamic-hero.tsx
@@ -1,4 +1,6 @@
 import Container from "@/components/layout/container";
+import { grotesk } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
@@ -16,9 +18,32 @@ const DynamicHero = () => {
           width={1800}
           height={1044}
           alt="image"
-          className="w-full h-[54.37vw]"
+          className="w-full sm:h-[54.37vw] h-[62.52vw] overflow-hidden object-cover"
         />
       </div>
+
+      <Container className={cn(grotesk.className, "text-WHITE")}>
+        <p className="mb-[10.25vw] text-[5.12] font-bold">
+          After working at global and national ad agencies, as well as our own
+          studios, we can tell you that much of the creative business is built
+          around bloat.
+        </p>
+        <div className="paragraph flex flex-col gap-[4.05vw] mb-[10.25vw]">
+          <div>
+            Agencies love shrouding the branding process in mystery. Fancy it
+            up. Drag it out. Layer it with sign-offs and check-in meetings. But
+            much of what happens behind the curtain is merely process for the
+            sake of process. It’s the fluff that inflates fees.
+          </div>
+          <div>
+            The complexity is built into the architecture, and clients are
+            paying for it with every billable hour.
+          </div>
+        </div>
+
+        <h5 className="paragraph !text-WHITE">Subscriptions slash waste.</h5>
+        <div className="flex flex-col gap-[4.02vw]"></div>
+      </Container>
     </section>
   );
 };
