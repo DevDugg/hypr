818d5a3a6b4f07315cb633930e2589e420315d5d
DevDugg <mr.aydogdy02@gmail.com>
2024-09-09 01:35:28 +0500

    images added on mobile

--- src/components/sections/our-services/service-card-desktop.tsx
+++ src/components/sections/our-services/service-card-desktop.tsx
@@ -1,36 +1,47 @@
 "use client";
 
+import Image from "next/image";
 import { motion } from "framer-motion";
 
 interface IProps {
   title: string;
   i: number;
   activeItem: number | null;
+  img: string;
 }
 
-export const ServiceCardDesktop = ({ title, i, activeItem }: IProps) => {
+export const ServiceCardDesktop = ({ title, i, activeItem, img }: IProps) => {
   const itemState = activeItem !== null ? (activeItem === i ? { opacity: "100%" } : { opacity: "30%" }) : {};
   return (
-    <motion.div
-      id={`element-${i}`}
-      className="relative overflow-hidden h-[3.75vw] w-full"
-      initial={{ opacity: "100%" }}
-      animate={itemState}
-    >
-      <motion.h4
-        className="leading-none monument uppercase w-full text-[4.61vw] sm:size72 absolute top-0 text-center origin-bottom tracking-tight"
-        initial={{ top: "100%", rotateX: 45, transformOrigin: "top" }}
-        animate={activeItem === i ? { top: "0%", rotateX: 0, transformOrigin: "bottom" } : {}}
+    <div className="flex flex-col gap-4">
+      <motion.div
+        id={`element-${i}`}
+        className="relative overflow-hidden h-[3.75vw] w-full"
+        initial={{ opacity: "100%" }}
+        animate={itemState}
       >
-        {title}
-      </motion.h4>
-      <motion.span
-        className="leading-none monument uppercase w-full text-[4.61vw] sm:size72 absolute top-0 text-center origin-bottom tracking-tight"
-        initial={{ top: "0%", rotateX: 0, transformOrigin: "bottom" }}
-        animate={activeItem === i ? { top: "-100%", rotateX: 45, transformOrigin: "top" } : {}}
+        <motion.h4
+          className="leading-none monument uppercase w-full text-[4.61vw] sm:size72 absolute top-0 text-center origin-bottom tracking-tight"
+          initial={{ top: "100%", rotateX: 45, transformOrigin: "top" }}
+          animate={activeItem === i ? { top: "0%", rotateX: 0, transformOrigin: "bottom" } : {}}
+        >
+          {title}
+        </motion.h4>
+        <motion.span
+          className="leading-none monument uppercase w-full text-[4.61vw] sm:size72 absolute top-0 text-center origin-bottom tracking-tight"
+          initial={{ top: "0%", rotateX: 0, transformOrigin: "bottom" }}
+          animate={activeItem === i ? { top: "-100%", rotateX: 45, transformOrigin: "top" } : {}}
+        >
+          {title}
+        </motion.span>
+      </motion.div>
+      <motion.div
+        initial={{ height: 0, opacity: 0 }}
+        animate={i === activeItem ? { height: "fit-content", opacity: 1 } : {}}
+        className="hidden max-md:flex max-md:justify-center"
       >
-        {title}
-      </motion.span>
-    </motion.div>
+        <Image src={img} width={256} height={320} alt="service image" className="size-full w-1/2 object-cover" />
+      </motion.div>
+    </div>
   );
 };
