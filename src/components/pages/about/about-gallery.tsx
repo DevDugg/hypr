3841794e884d869540359bd4b3ed9304599c0d1b
DevDugg <mr.aydogdy02@gmail.com>
2024-09-09 01:17:56 +0500

    Refactor smooth-scroll component

--- src/components/pages/about/about-gallery.tsx
+++ src/components/pages/about/about-gallery.tsx
@@ -3,6 +3,50 @@ import CustomButton from "@/components/shared/custom-button";
 import Image from "next/image";
 
 const AboutGallery = () => {
+  const images = [
+    {
+      src: "/images/case/gallery/1.png",
+      alt: "gallery image",
+      width: 524,
+      height: 350,
+      className: "absolute top-0 left-0 h-[18.22vw] w-[27.29vw] hover:cursor-grab",
+    },
+    {
+      src: "/images/case/gallery/2.png",
+      alt: "",
+      width: 524,
+      height: 350,
+      className: "absolute bottom-0 left-0 h-[18.22vw] w-[27.29vw] z-[4]",
+    },
+    {
+      src: "/images/case/gallery/3.png",
+      alt: "",
+      width: 413,
+      height: 620,
+      className: "absolute top-0 left-[24.58vw] z-[3] h-[32.29vw] w-[21.51vw]",
+    },
+    {
+      src: "/images/case/gallery/4.png",
+      alt: "",
+      width: 709,
+      height: 398,
+      className: "absolute top-0 right-[14.68vw] z-[3] h-[20.41vw] w-[36.92vw]",
+    },
+    {
+      src: "/images/case/gallery/5.png",
+      alt: "",
+      width: 600,
+      height: 452,
+      className: "absolute bottom-0 right-[18.22vw] z-[2] h-[23.54vw] w-[31.25vw]",
+    },
+    {
+      src: "/images/case/gallery/6.png",
+      alt: "",
+      width: 392,
+      height: 586,
+      className: "absolute top-0 right-0 z-[1] h-[30.52vw] w-[20.41vw]",
+    },
+  ];
   return (
     <section>
       <Container>
