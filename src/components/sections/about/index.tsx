857c5ed2f670b90c3e9fc56de9879ad91c564f06
DevDugg <mr.aydogdy02@gmail.com>
2024-09-12 04:04:16 +0500

    about displayed

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -1,31 +1,23 @@
 import Container from "@/components/layout/container";
+import { HOME_PAGE_QUERYResult } from "../../../../sanity.types";
 import Paragraph from "./character";
 import SectionName from "@/components/titles/section-name";
 import { cn } from "@/lib/utils";
 import { grotesk } from "@/lib/fonts";
 
-const About = () => {
-  const paragraph = `HYPR agency is a branding and digital marketing studio from Australia.
-        Partnering with companies that need to transform their brand, attract
-        the right audience, and radically stand out. We’re a nimble team of
-        designers and creative developers, focused on creating uniquely human
-        and culturally informed.`;
+interface AboutProps {
+  about: NonNullable<HOME_PAGE_QUERYResult[number]["about"]>;
+}
+
+const About = ({ about }: AboutProps) => {
+  const { subtitle, text } = about;
 
   return (
     <section>
       <Container className="relative">
-        <SectionName className="absolute top-[3.07vw] sm:top-[2.08vw] left-0">
-          (about)
-        </SectionName>
-        <div
-          className={cn(
-            "about-text flex transition-colors text-WHITE duration-200 relative",
-            grotesk.className
-          )}
-        >
-          <div className="font-semibold text-[1vw]">
-            <Paragraph value={paragraph} />
-          </div>
+        {subtitle && <SectionName className="absolute top-[3.07vw] sm:top-[2.08vw] left-0">{subtitle}</SectionName>}
+        <div className={cn("about-text flex transition-colors text-WHITE duration-200 relative", grotesk.className)}>
+          <div className="font-semibold text-[1vw]">{text && <Paragraph value={text} />}</div>
         </div>
       </Container>
     </section>
