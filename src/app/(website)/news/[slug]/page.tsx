10ac06f84ee62945a89e59a7537ce1451b6d8fd6
DevDugg <mr.aydogdy02@gmail.com>
2024-09-13 18:38:24 +0500

    clg removed

--- src/app/(website)/news/[slug]/page.tsx
+++ src/app/(website)/news/[slug]/page.tsx
@@ -11,7 +11,6 @@ interface NewsItemPageParams {
 const NewsItemPage = async ({ params }: NewsItemPageParams) => {
   const { slug } = params;
   const news = await getNewsItemData(slug);
-  console.log(news);
   return (
     <main className="flex flex-col gap-[20.5vw] sm:gap-[5.2vw] mt-[10.25vw] sm:mt-[5.2vw]">
       <DynamicHero item={news} />
