12692fbbc92da8d3f9f209a5594e5ccec51043f9
Aydogdy Agabayev <mr.aydogdy02@gmail.com>
2024-10-03 15:55:13 +0500

    google analytics added

--- src/config/provider.tsx
+++ src/config/provider.tsx
@@ -2,6 +2,7 @@
 
 import AnimationContextProvider from "@/context/animation-context";
 import CheckConfig from "./check-config";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { MotionConfig } from "framer-motion";
 import { PropsWithChildren } from "react";
 import SmoothScroll from "@/components/animations/smooth-scroll";
@@ -10,17 +11,20 @@ import { defaultTransition } from "./transitions";
 
 const Provider = ({ children }: PropsWithChildren) => {
   return (
-    <SmoothScroll>
-      <MotionConfig transition={defaultTransition}>
-        {/* <ScrollProgress> */}
-        <AnimationContextProvider>
-          <ViewTransitions>
-            <CheckConfig>{children}</CheckConfig>
-          </ViewTransitions>
-        </AnimationContextProvider>
-        {/* </ScrollProgress> */}
-      </MotionConfig>
-    </SmoothScroll>
+    <>
+      <SmoothScroll>
+        <MotionConfig transition={defaultTransition}>
+          {/* <ScrollProgress> */}
+          <AnimationContextProvider>
+            <ViewTransitions>
+              <CheckConfig>{children}</CheckConfig>
+            </ViewTransitions>
+          </AnimationContextProvider>
+          {/* </ScrollProgress> */}
+        </MotionConfig>
+      </SmoothScroll>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_TRACKING_ID || ""} />
+    </>
   );
 };
 export default Provider;
