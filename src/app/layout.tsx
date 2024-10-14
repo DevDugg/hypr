dfcabe9a8881fddb24dfb150ee4a70628fd1e14d
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-10-14 13:46:52 +0500

    datafast scipt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 
-import Loader from "@/components/sections/loader";
-import Provider from "@/config/provider";
+import Script from "next/script";
 import { Viewport } from "next";
 import { colors } from "@/config/colors";
 import { getSEOTags } from "@/lib/seo";
@@ -26,6 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${monument.className} bg-BACKGROUND w-screen h-screen overflow-x-hidden`}>{children}</body>
+      <Script
+        defer
+        data-website-id="670cda2cbcb7e9de563c18a9"
+        data-domain="createdbyhypr.com"
+        src="https://datafa.st/js/script.js"
+      ></Script>
     </html>
   );
 }
