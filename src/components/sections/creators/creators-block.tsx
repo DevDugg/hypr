01ac7e5b5fda63dd04c67bfe776614e7ef90d0c1
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-10-15 01:53:22 +0500

    carousel added

--- src/components/sections/creators/creators-block.tsx
+++ src/components/sections/creators/creators-block.tsx
@@ -4,7 +4,6 @@ import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carouse
 
 import { CreatorsCard } from "./creators-card";
 import { HOME_PAGE_QUERYResult } from "../../../../sanity.types";
-import { Link } from "next-view-transitions";
 
 interface CreatorsBlockProps {
   creators: NonNullable<HOME_PAGE_QUERYResult[number]["creators"]>["items"];
@@ -15,30 +14,23 @@ const CreatorsBlock = ({ creators }: CreatorsBlockProps) => {
     creators &&
     creators.length > 0 && (
       <>
-        <div className="mt-[3.33vw] hidden sm:flex gap-[2.56vw] sm:gap-[0.5vw]">
+        {/* <div className="mt-[3.33vw] hidden sm:flex gap-[2.56vw] sm:gap-[0.5vw]">
           {creators.map((item, i) => (
             // <Reveal key={i} animateOnView delay={i * 0.2} duration={1}>
             <CreatorsCard {...item} key={i} />
             // </Reveal>
           ))}
-        </div>
+        </div> */}
 
-        <Carousel className="sm:hidden" opts={{ skipSnaps: true }}>
+        <Carousel opts={{ skipSnaps: true, breakpoints: {} }}>
           <CarouselContent>
             {creators.map((item, i) => (
-              <CarouselItem key={i} className="basis-[1/2]">
+              <CarouselItem key={i} className="basis-[1/4] max-sm:basis-[1/2]">
                 <CreatorsCard {...item} key={i} />
               </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
-
-        <Link
-          // href={"/creators"}
-          href={"/"}
-        >
-          {/* <CustomButton className="w-full mt-[8.1vw] sm:mt-[2.08vw]">See all creators</CustomButton> */}
-        </Link>
       </>
     )
   );
