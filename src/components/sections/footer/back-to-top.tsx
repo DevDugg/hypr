d73e166d6919b8888a445edc19fea5dd4b6cfed5
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-11-02 11:04:47 +0500

    arrow changed

--- src/components/sections/footer/back-to-top.tsx
+++ src/components/sections/footer/back-to-top.tsx
@@ -1,20 +1,21 @@
 "use client";
 
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { grotesk } from "@/lib/fonts";
 import { useScrollToTarget } from "@/lib/use-scroll-to-target";
 
 const BackToTop = () => {
-  const { handleOnClick } = useScrollToTarget("#hero");
+  const { handleOnClick } = useScrollToTarget("#top");
   return (
     <div
       onClick={handleOnClick}
       className={cn(
         "flex items-center cursor-pointer sm:font-bold font-medium text-[3.58vw] sm:size24 justify-center gap-[0.2vw]",
-        grotesk.className
+        grotesk.className,
       )}
     >
-      Back to top ↑
+      Back To Top <Image src="/images/arrow-up.svg" alt="Arrow up" width={24} height={24} className="size-[1.25vw]" />
     </div>
   );
 };
