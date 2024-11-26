e34c852dedfd5f1d734d096809c8513473280ca2
atdayev <atdayewkemal@gmail.com>
2024-11-27 01:35:46 +0500

    added: more creators btn

--- src/components/sections/creators/creators-block.tsx
+++ src/components/sections/creators/creators-block.tsx
@@ -3,7 +3,9 @@
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 
 import { CreatorsCard } from "./creators-card";
+import CustomButton from "@/components/shared/custom-button";
 import { HOME_PAGE_QUERYResult } from "../../../../sanity.types";
+import { Link } from "next-view-transitions";
 
 interface CreatorsBlockProps {
   creators: NonNullable<HOME_PAGE_QUERYResult[number]["creators"]>["items"];
@@ -14,23 +16,28 @@ const CreatorsBlock = ({ creators }: CreatorsBlockProps) => {
     creators &&
     creators.length > 0 && (
       <>
-        {/* <div className="mt-[3.33vw] hidden sm:flex gap-[2.56vw] sm:gap-[0.5vw]">
+        <div className="mt-[3.33vw] hidden sm:flex gap-[2.56vw] sm:gap-[0.5vw]">
           {creators.map((item, i) => (
             // <Reveal key={i} animateOnView delay={i * 0.2} duration={1}>
             <CreatorsCard {...item} key={i} />
             // </Reveal>
           ))}
-        </div> */}
+        </div>
 
         <Carousel opts={{ skipSnaps: true, breakpoints: {} }}>
           <CarouselContent>
             {creators.map((item, i) => (
-              <CarouselItem key={i} className="basis-[1/4] max-sm:basis-[1/2]">
+              <CarouselItem key={i} className="hidden basis-[1/2] max-sm:block">
                 <CreatorsCard {...item} key={i} />
               </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
+        <CustomButton fullWidth className="mt-[2.08vw]">
+          <Link href={"/creators"} className="uppercase">
+            More creators
+          </Link>
+        </CustomButton>
       </>
     )
   );
--- src/components/sections/creators/creators-card.tsx
+++ src/components/sections/creators/creators-card.tsx
@@ -1,5 +1,6 @@
 import { HOME_PAGE_QUERYResult } from "../../../../sanity.types";
 import Image from "next/image";
+import { Link } from "next-view-transitions";
 import { cn } from "@/lib/utils";
 import { grotesk } from "@/lib/fonts";
 import { urlFor } from "@/sanity/lib/image";
@@ -10,40 +11,43 @@ export const CreatorsCard = ({
   image,
   social_media_1,
   social_media_2,
+  slug,
 }: NonNullable<NonNullable<HOME_PAGE_QUERYResult[number]["creators"]>["items"]>[number]) => {
   return (
-    <div className="w-[51.25vw] sm:w-full">
-      {image && (
-        <Image
-          src={urlFor(image).size(1026, 1440).url()}
-          alt="news image"
-          width={1026}
-          height={1440}
-          className="w-full h-[66.62vw] sm:h-[25vw] object-cover"
-        />
-      )}
-
-      <div className="mt-[4.05vw] sm:mt-[1.66vw] mb-[4.05vw] sm:mb-[1.25vw] text-[4.05vw] sm:text-[1.25vw] monument">
-        {creator_name}
-      </div>
-      <p
-        className={cn(
-          "leading-[140%] text-[4.05vw] sm:text-[1.04vw] mb-[1.02vw] sm:mb-[0.416vw] font-normal",
-          grotesk.className,
+    <Link href={`/creators/${slug?.current}`} className="w-full">
+      <div className="w-[51.25vw] sm:w-full">
+        {image && (
+          <Image
+            src={urlFor(image).size(1026, 1440).url()}
+            alt="news image"
+            width={1026}
+            height={1440}
+            className="w-full h-[66.62vw] sm:h-[32vw] object-cover"
+          />
         )}
-      >
-        {handle}
-      </p>
 
-      <div
-        className={cn(
-          "flex flex-col text-[4.05vw] sm:text-[1.04vw] gap-[1.02vw] sm:gap-[0.41vw] leading-[140%]",
-          grotesk.className,
-        )}
-      >
-        {social_media_1 && <p>{social_media_1}</p>}
-        {social_media_2 && <p>{social_media_2}</p>}
+        <div className="mt-[4.05vw] sm:mt-[1.66vw] mb-[4.05vw] sm:mb-[1.25vw] text-[4.05vw] sm:text-[1.25vw] monument">
+          {creator_name}
+        </div>
+        <p
+          className={cn(
+            "leading-[140%] text-[4.05vw] sm:text-[1.04vw] mb-[1.02vw] sm:mb-[0.416vw] font-normal",
+            grotesk.className,
+          )}
+        >
+          {handle}
+        </p>
+
+        <div
+          className={cn(
+            "flex flex-col text-[4.05vw] sm:text-[1.04vw] gap-[1.02vw] sm:gap-[0.41vw] leading-[140%]",
+            grotesk.className,
+          )}
+        >
+          {social_media_1 && <p>{social_media_1}</p>}
+          {social_media_2 && <p>{social_media_2}</p>}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
