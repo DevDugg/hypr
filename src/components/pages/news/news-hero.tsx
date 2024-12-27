443f040547158f1241e9c123a82bd167363dc912
DevDugg <mr.aydogdy02@gmail.com>
2024-12-28 01:22:05 +0500

    Update NewsHero component: change title element from h1 to h2 and update description element from p to h1 for improved semantics and styling.

--- src/components/pages/news/news-hero.tsx
+++ src/components/pages/news/news-hero.tsx
@@ -6,11 +6,11 @@ const NewsHero = async () => {
   const data = await getNewsPageData();
   return (
     <section className="mb-[10.25vw] sm:mb-[7.81vw] mt-[10.25vw] sm:mt-0">
-      <h1 className="text-[11.45vw] text-center text-ACCENT uppercase font-bold">insights</h1>
+      <h2 className="text-[11.45vw] text-center text-ACCENT uppercase font-bold">insights</h2>
       {data && (
-        <p className={cn("text-center text-WHITE font-semibold text-[4.1vw] sm:size40", grotesk.className)}>
+        <h1 className={cn("text-center text-WHITE font-semibold text-[4.1vw] sm:size40", grotesk.className)}>
           {data.description}
-        </p>
+        </h1>
       )}
     </section>
   );
