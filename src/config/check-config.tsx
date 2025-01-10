1be330a6e903bfb6ee1b630d65e102a8360455e2
DevDugg <mr.aydogdy02@gmail.com>
2025-01-10 16:53:03 +0500

    fixed scroll issues

--- src/config/check-config.tsx
+++ src/config/check-config.tsx
@@ -2,9 +2,20 @@
 
 import { PropsWithChildren, useEffect } from "react";
 
+import { useLenis } from "@studio-freight/react-lenis";
+import { usePathname } from "next/navigation";
 import validateConfig from "@/lib/validate-config";
 
 const CheckConfig = ({ children }: PropsWithChildren) => {
+  const path = usePathname();
+  const lenisInstance = useLenis();
+  useEffect(() => {
+    if (!lenisInstance) {
+      return window.scrollTo(0, 0);
+    }
+    lenisInstance.scrollTo(0, { immediate: true, force: true });
+  }, [path]);
+
   useEffect(() => {
     validateConfig();
   }, []);
